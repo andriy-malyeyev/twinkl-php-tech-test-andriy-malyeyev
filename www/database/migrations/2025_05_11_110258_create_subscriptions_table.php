d29be9698e9dcc796f3e0cf8fcc758f53589c0d0
Andriy Malyeyev <malyeyev.andriy.mykolayovych@gmail.com>
2025-05-11 14:08:22 +0300

    Create the subscription table.

---
+++ www/database/migrations/2025_05_11_110258_create_subscriptions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', static function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->enum('role', ['student', 'teacher', 'parent', 'private_tutor']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
