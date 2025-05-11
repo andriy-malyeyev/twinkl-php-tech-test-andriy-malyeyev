6a93045d57a2a30300417c1fef7899fa6097c172
Andriy Malyeyev <malyeyev.andriy.mykolayovych@gmail.com>
2025-05-11 14:28:52 +0300

    Add teh subscription model.

---
+++ www/app/Models/Subscription.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'role',
+    ];
+}
