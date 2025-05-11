cc6687488e340daedf152a22217aacb21b9978dc
Andriy Malyeyev <malyeyev.andriy.mykolayovych@gmail.com>
2025-05-11 14:36:00 +0300

    Add create action for the subscriber.

--- www/app/Http/Controllers/API/Subscription/SignUpController.php
+++ www/app/Http/Controllers/API/Subscription/SignUpController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API\Subscription;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscription;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SignUpController extends Controller
@@ -13,10 +15,18 @@ class SignUpController extends Controller
     /**
      * Handles the creation of a new subscription.
      *
+     * @param  Request  $request  The request.
      * @return JsonResponse JSON response.
      */
-    public function store(): JsonResponse
+    public function store(Request $request): JsonResponse
     {
+        Subscription::create([
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'email' => $request->get('email'),
+            'role' => $request->get('role'),
+        ]);
+
         return response()->json([
             'status' => 'success',
             'message' => 'Subscription created successfully.',
